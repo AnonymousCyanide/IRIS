4df2ae0c1e3f9cbda9391b36a930fafd9ef7b8e3
Avim <avidubey2000@gmail.com>
2022-07-15 00:16:08 +0530

    Added add post function

--- DB.py
+++ DB.py
@@ -1,13 +1,19 @@
 import mysql.connector
 import json
 from RedCom import Reddit
+def check_ele(l,s):
+    for i in l:
+        if i in s:
+            return True
+    return False
 
-
-class DB():
-    def __int__():
+class DataBase():
+    def __init__(self):
+        print('object made')
         data = None
         with open('cred.json') as f:
             data = json.load(f)
+            print(data)
         host = data['SqlCred']['host']
         user = data['SqlCred']['user']
         pwd = data['SqlCred']['pwd']
@@ -17,12 +23,14 @@ class DB():
                 user = user,
                 password = pwd
             )
-            self.cursor = self.mydb.cursor()
+            self.cursor = self.mydb.cursor(buffered = True)
             self.cursor.execute('USE iris')
+            print('Done')
         except :
             self.__del__()
     def __del__(self):
         print('Object Destroyed')
+    # Update susbcriber count in subreddit
     def refresh_subreddit(self):
         self.cursor.execute('SELECT * FROM subreddits')
         subreddits = []
@@ -41,13 +49,53 @@ class DB():
             self.cursor.execute(query)
             self.mydb.commit()
             
-
-    def get_subs():
-        cursor.execute('SELECT * FROM subreddits')
+    # gets a list of all subs 
+    def get_subs(self):
+        self.cursor.execute('SELECT * FROM subreddits')
         subreddits = []
-        for x in cursor:
+        for x in self.cursor:
             #print(x)
             subreddits.append(x[0])
         return subreddits
+    #adds to posts table in data base
+    def add_post(self,post,sub):
+        id = post['kind'] + '_' + post['data']['id']
+        query = f'SELECT * FROM posts WHERE id = "{id}"'
+        self.cursor.execute(query)
+        if self.cursor.rowcount != 0:
+            print('post already here')
+            return None
+        try :
+            content  = post['data']['selftext']
+            if len(content) > 200 or check_ele(['"',"'"],content):
+
+                with open(f'content/{id}.txt','w') as f:
+                    f.write(content)
+                    content = f'content/{id}.txt'
+
+
+            query = f''' 
+            INSERT INTO posts VALUES(
+                    "{id}",
+                    "{sub}",
+                    "{post['data']['title']}",
+                    "{content}",
+                    "{post['data']['upvote_ratio']}",
+                    "{post['data']['score']}"
+                                            );
+                    '''
+            try :
+                self.cursor.execute(query)
+                self.mydb.commit()
+                print('posts added sub :',sub)
+            except :
+                print('Query error')
+                if sub == 'BlankPagesEmptyMugs':
+                    print(query)
+        except :
+            print('error in keys ',sub)
+
+       
+         
 
 
